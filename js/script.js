2bb968f51e40c0694818aeb511a800423d5d9c1c
LuanEduardoS <luaneduardolima186@gmail.com>
2022-05-14 12:59:14 -0300

    .
    algumas mudanças no nav e cor de background

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var n = window.prompt('Será que vai da certo? Responda com S OU N.')
-        var s = 'S'
-        
-        if  (n == s) {
-            alert('Deu bom!')
-        } else {
-            alert('F')
-        } 
