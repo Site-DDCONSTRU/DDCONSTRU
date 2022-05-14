5e8fe25188954d4c5e0247b76dc32a787c631796
Dannyeclisson <dannyeclissonrodrigo@gmail.com>
2022-05-14 11:28:56 -0300

    Usando Script externo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var n = window.prompt('Será que vai da certo? Responda com S OU N.')
+        var s = 'S'
+        
+        if  (n == s) {
+            alert('Deu bom!')
+        } else {
+            alert('F')
+        } 
